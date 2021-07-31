603102ef34fc4fe09ab57cdbde7037061102eb8b
Sobraloser <Asgoth55@gmail.com>
2021-07-30 23:27:51 -0300

    feat: add parseJWT

--- src/utils/handleToken.js
+++ src/utils/handleToken.js
@@ -1,11 +1,11 @@
 export const TOKEN_KEY = "@wpp-Token";
 
-export const isAuthenticated = () => sessionStorage.getItem(TOKEN_KEY) !== null;
-export const getToken = () => sessionStorage.getItem(TOKEN_KEY);
+export const isAuthenticated = () => localStorage.getItem(TOKEN_KEY) !== null;
+export const getToken = () => localStorage.getItem(TOKEN_KEY);
 
 export const login = token => {
-  sessionStorage.setItem(TOKEN_KEY, token);
+  localStorage.setItem(TOKEN_KEY, token);
 };
 export const logout = () => {
-  sessionStorage.removeItem(TOKEN_KEY);
+  localStorage.removeItem(TOKEN_KEY);
 };
