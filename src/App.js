f843c7ddaecba741033ac2e93e8bc475d22d24c4
Sobraloser <Asgoth55@gmail.com>
2021-08-01 13:18:50 -0300

    refact: move UsersContext to index

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { UsersProvider } from './contexts/UsersContext.js';
 import Routes from './routes.js'
 import './styles/global.scss'
 
 export default function App() {
   return (
     <div className="App">
-      <Routes />
+      <UsersProvider>
+        <Routes />
+      </UsersProvider>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,14 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { AuthProvider } from './contexts/AuthContext';
-import { UsersProvider } from './contexts/UsersContext';
 
 ReactDOM.render(
   <React.StrictMode>
     <AuthProvider>
-      <UsersProvider>
-        <App />
-      </UsersProvider>   
+      <App />
     </AuthProvider>
   </React.StrictMode>,
   document.getElementById('root')
