fa96705babaee26f70cfdc1e5dd8f1245d6878ab
Sobraloser <Asgoth55@gmail.com>
2021-08-02 22:17:11 -0300

    refact: change fetch rooms to Main page

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react'
 import { Chat } from '../../components/Chat/index.js'
 import { SideBar } from '../../components/SideBar'
 import { useUsers } from '../../contexts/UsersContext.js'
@@ -5,7 +6,14 @@ import '../../services/socket.js'
 import styles from './styles.module.scss'
 
 export function Main(){
-  const { selectedRoom } = useUsers()
+  const { selectedRoom, handleFetchRooms } = useUsers()
+
+  useEffect(() => {
+    (async function(){
+      await handleFetchRooms()
+    })()
+  },[handleFetchRooms])
+
   return(
     <div className={styles.main}>
       <aside>
