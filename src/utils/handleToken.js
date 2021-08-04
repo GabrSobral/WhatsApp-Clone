1e61eb754a27a8c1513beb7caa9c60877066e8f5
Sobraloser <Asgoth55@gmail.com>
2021-08-04 08:54:46 -0300

    refact: change localStorage to sessionStorage

--- src/utils/handleToken.js
+++ src/utils/handleToken.js
@@ -1,11 +1,11 @@
 export const TOKEN_KEY = "@wpp-Token";
 
-export const isAuthenticated = () => localStorage.getItem(TOKEN_KEY) !== null;
-export const getToken = () => localStorage.getItem(TOKEN_KEY);
+export const isAuthenticated = () => sessionStorage.getItem(TOKEN_KEY) !== null;
+export const getToken = () => sessionStorage.getItem(TOKEN_KEY);
 
 export const login = token => {
-  localStorage.setItem(TOKEN_KEY, token);
+  sessionStorage.setItem(TOKEN_KEY, token);
 };
 export const logout = () => {
-  localStorage.removeItem(TOKEN_KEY);
+  sessionStorage.removeItem(TOKEN_KEY);
 };
