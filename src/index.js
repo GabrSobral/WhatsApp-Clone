6d951989ec17b273195b6c06e751d497b049e461
Sobraloser <Asgoth55@gmail.com>
2021-08-05 23:03:12 -0300

    feat: remove React.StrictMode

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import App from './App';
 import { AuthProvider } from './contexts/AuthContext';
 
 ReactDOM.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <AuthProvider>
       <App />
-    </AuthProvider>
-  </React.StrictMode>,
+    </AuthProvider>,
+  // </React.StrictMode>,
   document.getElementById('root')
 );
 
