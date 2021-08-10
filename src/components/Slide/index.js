1c62889e24a9a8a5ec2f8ba57513a346ad86eae6
Sobraloser <Asgoth55@gmail.com>
2021-08-10 20:03:51 -0300

    feat: add slide animation

---
+++ src/components/Slide/index.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+import styles from './styles.module.scss'
+
+export function Slide({ show, children }){
+  const [ shouldRender, setRender ] = useState(show);
+
+  useEffect(() => {
+    console.log('banananaaa', show)
+    if (show){ setRender(true) };
+  }, [show]);
+
+  const onAnimationEnd = () => {
+    if (!show){ setRender(false) };
+  };
+
+  return (
+    shouldRender && (
+      <div
+        className={`${styles.slide} `}
+        style={{animation: `${show ? styles.slideIn : styles.slideOut} 0.5s`}}
+        onAnimationEnd={onAnimationEnd}
+      >
+        {children}
+      </div>
+    )
+  );
+};
