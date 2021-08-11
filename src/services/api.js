74a3e2e73d6ec1564cf0afc841acef2e69ef012d
Sobraloser <Asgoth55@gmail.com>
2021-08-11 15:34:57 -0300

    console .env

--- src/services/api.js
+++ src/services/api.js
@@ -2,8 +2,9 @@ import axios from 'axios'
 import { getToken } from '../utils/handleToken';
 
 const api = axios.create({
-    baseURL : process.env.REACT_APP_API_URL
+  baseURL : process.env.REACT_APP_API_URL
 })
+console.log(process.env.REACT_APP_API_URL)
 
 const token = getToken();
 api.interceptors.request.use(async config => {
