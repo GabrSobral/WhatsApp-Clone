9547039bb78f6d9be528cb68f6e28deb6279c426
Sobraloser <Asgoth55@gmail.com>
2021-08-26 15:07:01 -0300

    feat: add other status states

--- src/contexts/StatusContext.js
+++ src/contexts/StatusContext.js
@@ -1,12 +1,35 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
+import api from '../services/api'
 
 const StatusContext = createContext(null)
 
 export function StatusProvider({ children }) {
+  const [ myStatus, setMyStatus ] = useState([])
+  const [ recentStatus, setRecentStatus ] = useState([])
+  const [ viewedStatus, setViewedStatus ] = useState([])
   const [ isStatusOpen, setIsStatusOpen ] = useState(false)
   const [ selectedStatus, setSelectedStatus ] = useState()
 
-  function handleStatusOpen(){ setIsStatusOpen(!isStatusOpen); setSelectedStatus(null) }
+  useEffect(() => {
+    (async () => {
+      const statusFetch = await api.get('/status/list')
+      const myStatusFetch = await api.get('/status/my-list')
+      setMyStatus(myStatusFetch.data)
+
+      statusFetch.data.forEach(item_data => {
+        if(item_data.status.indexOf(item => item.viewed) === -1) {
+          setRecentStatus(prevState => [ item_data, ...prevState])
+        } else {
+          setViewedStatus(prevState => [ item_data, ...prevState])
+        }
+      })
+    })()
+  },[])
+
+  function handleStatusOpen(){ 
+    setIsStatusOpen(!isStatusOpen); 
+    setSelectedStatus(null) 
+  }
   function handleSelectStatus(status){ setSelectedStatus(status) }
 
   return(
@@ -15,7 +38,10 @@ export function StatusProvider({ children }) {
         isStatusOpen,
         handleStatusOpen,
         selectedStatus,
-        handleSelectStatus
+        handleSelectStatus,
+        myStatus,
+        viewedStatus,
+        recentStatus
       }}
     >
       {children}
