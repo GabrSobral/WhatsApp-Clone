bc7031e69ebb5927ad5f35acb57d816dff94e864
Sobraloser <Asgoth55@gmail.com>
2021-10-08 10:34:54 -0300

    feat: implementind trackbar

--- src/components/SelectedStatus/HeaderSelectedStatus/TrackBar/index.js
+++ src/components/SelectedStatus/HeaderSelectedStatus/TrackBar/index.js
@@ -3,39 +3,46 @@ import { useStatus } from '../../../../contexts/StatusContext'
 import styles from './styles.module.scss'
 
 export function Trackbar(){
-  const { index, handleIndex, selectedStatus, handleSelectStatus } = useStatus()
+  const { index, handleIndex, selectedStatus } = useStatus()
   const [ percentage, setPercentage ] = useState(0)
   const [ finish, setFinish ] = useState(false)
   const maxDuration = 7 * 1000
-
+  
+  useEffect(() => { 
+    // console.log('percentage: ', percentage)
+    console.log('index: ', index)
+  },[ index, percentage ])
+  
   useEffect(() => {
+
     setTimeout(() => { setFinish(true) }, maxDuration)
     setPercentage(100)
-  },[maxDuration])
+  },[ maxDuration, index ])
 
   useEffect(() => {
     if(finish){
       handleIndex('next')
-      // setPercentage(0)
       setFinish(false)
     }
-  }, [finish, handleIndex])
-
-  useEffect(() => { console.log(percentage, index) },[ percentage, index ])
+  }, [ finish, handleIndex ])
 
   return(
     <div className={styles.track_bar}>
-      { selectedStatus.status.map((item, i) => 
-        <div className={styles.track_bar_item} key={item._id}>
-          <div style={
-            { width: `${
-                i < index ? 100 :
-                  i === index ? percentage : 0}%`,
-              transition: `${
-                i < index ? 'none' :
-                  i === index ? maxDuration / 1000 : 'none'}s` }}
-          />
-        </div>
+      { selectedStatus.status.map((item, i) => {
+        return (
+          <div className={styles.track_bar_item} key={item._id}>
+            <div style={
+              { width: `${
+                  i < index ? 100 :
+                    i === index ? percentage : 0 }%`,
+                transition: `${
+                  i < index ? 'none' :
+                    i === index ? `${maxDuration/1000}s` : 'none'}` }}
+            />
+          </div>
+        )
+      }
+        
       ) }
     </div>
   )
