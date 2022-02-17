4b7cb7ccfb5f8d2fdd557d26d4f0df2246d29ca0
Sobraloser <Asgoth55@gmail.com>
2022-02-17 12:34:16 -0300

    refact: SideBar UserItem

--- src/components/SideBar/UserItem/index.tsx
+++ src/components/SideBar/UserItem/index.tsx
@@ -1,11 +1,13 @@
-// import { MdDoneAll, MdPerson } from 'react-icons/md'
-// import { FaAngleDown } from 'react-icons/fa'
-import styles from './styles.module.scss'
 import { format } from 'date-fns'
 import { useAuth } from '../../../contexts/AuthContext'
 import { useRooms } from '../../../contexts/RoomsContext'
 import { SendingSVG } from '../../../images/sending'
 import { IRoom } from '../../../types/IRoom'
+import { PersonSVG } from '../../../images/person'
+import { ArrowDown } from '../../../images/arrow_down'
+import { DoubleCheckSVG } from '../../../images/double_check'
+
+import styles from './styles.module.scss'
 
 type Props = {
   room: IRoom;
@@ -20,9 +22,10 @@ export function UserItem({ room, index }: Props){
     format(new Date(lastMessage.timestamp), "HH:mm")
 
   return(
-    <div className={styles.user_container} onClick={() => handleSelectRoom(index)}>
+    <div onClick={() => handleSelectRoom(index)} className={`${styles.user_container} 
+        ${selectedRoom?._id === room._id && styles.active}`}>
       <div className={`${styles.user_img} ${room.user[0].isOnline && styles.online}`}>  
-        {/* <MdPerson size={30} fill={room.user[0].isOnline ? '#51b786' : '#919191'}/>  */}
+        <PersonSVG color={room.user[0].isOnline ? '#51b786':'#b5b5b5'}/> 
       </div>
 
       <div className={styles.container_char}>
@@ -34,28 +37,19 @@ export function UserItem({ room, index }: Props){
         <div className={styles.message_and_counter}>
           { room.isWritting ? 
             <span className={styles.writting}>typing...</span> : 
-            <span className={`${styles.lastMessage} ${!isFocused && styles.blur}`}>
-              {room.messages.length !== 0 && 
-                (
-                  lastMessage.user !== myId ? 
-                  lastMessage?.message :
-                  <>
-                    {!lastMessage.received ? 
-                      <SendingSVG/> :
-                      <div/>
-                      // <MdDoneAll size={19} fill={lastMessage?.viewed ? "#30B1E7" : "#A6ADA0"}/>
-                    } 
-                    {lastMessage?.message}
-                  </>
-                )
-              }
-            </span>
+            <div className={`${styles.lastMessage} ${!isFocused && styles.blur}`}>
+              {lastMessage?.user === myId && (lastMessage.received ?
+                <DoubleCheckSVG color={lastMessage?.viewed ? "#30B1E7":"#b1b1b1"} size={18}/> : 
+                <SendingSVG color="#b1b1b1"/>)}
+              <span>{lastMessage?.message}</span>
+            </div>
           }
 
-          {room.unreadMessages !== 0 && <div>{room?.unreadMessages}</div>}
+          {room.unreadMessages !== 0 && 
+            <span className={styles.message_counter}>{room?.unreadMessages}</span>}
 
-          <button type="button" className={styles.details}>
-            {/* <FaAngleDown size={20} fill="#919191"/> */}
+          <button type="button" className={styles.details} onClick={()=> alert()}>
+            <ArrowDown size={15} color="#b1b1b1"/>
           </button>
         </div>
       </div>
